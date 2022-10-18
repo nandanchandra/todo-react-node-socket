798ad55eced960c0bcd150af6b3c292f6e30b596
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-10-18 23:07:54 +0530

    navbar

---
+++ client/src/Components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/navbar.css";
+
+const Nav = () => {
+	return (
+		<nav className="navbar">
+			<h2>Todo List</h2>
+		</nav>
+	);
+};
+
+export default Nav;
